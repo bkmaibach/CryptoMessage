7907c89b75a2518536993d5f534582af41447228
bmaib_000 <keith@maibach.ca>
2017-01-01 11:50:37 -0800

    Fix EncryptorTest by combining all test functions into one function

--- app/src/test/java/com/maibach/keith/cryptomessage/EncryptorTest.java
+++ app/src/test/java/com/maibach/keith/cryptomessage/EncryptorTest.java
@@ -12,6 +12,7 @@ import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import org.junit.Assert;
 import java.io.File;
+import java.util.Scanner;
 
 /**
  * Created by bmaib_000 on 2016-08-15.
@@ -24,8 +25,8 @@ public class EncryptorTest {
     private static File publicKeyFile;
     private static File privateKeyFile;
     private static File aesKeyEncryptedFile;
-    private static File resavedKeyFile;
     private static File secondResultFile;
+
 
     @Before
     public void init()
@@ -50,8 +51,6 @@ public class EncryptorTest {
         publicKeyFile = new File("test/EncryptorTest/public.der");
         privateKeyFile = new File("test/EncryptorTest/private.der");
         aesKeyEncryptedFile = new File("test/EncryptorTest/aesKeyEncrypted.txt");
-        aesKeyEncryptedFile = new File("test/EncryptorTest/aesKeyEncrypted.txt");
-        resavedKeyFile = new File("test/EncryptorTest/resavedKey.txt");
         secondResultFile = new File("test/EncryptorTest/secondResult.txt");
 
         Assert.assertTrue(toEncryptFile.exists());
@@ -69,62 +68,31 @@ public class EncryptorTest {
     }
 
     @Test
-    public void key_getsMade() throws Exception
-    {
-        mEncryptor.makeKey();
-        Assert.assertTrue("key creation failed", mEncryptor.isInitialized());
-    }
-
-    @Test
-    public void encryptionDecryptionCheck()
+    public void EncryptDecryptReloadRecheck()
     {
         Assert.assertTrue(toEncryptFile.canRead());
         try {
+            mEncryptor.makeKey();
+            Assert.assertTrue("key creation failed", mEncryptor.isInitialized());
+
             mEncryptor.encrypt(toEncryptFile, toDecryptFile);
+            String startedWith = new Scanner(toEncryptFile).toString();
             mEncryptor.decrypt(toDecryptFile, resultFile);
-        } catch (IOException| InvalidKeyException e){
+            String endedWith = new Scanner(resultFile).toString();
+            Assert.assertEquals("String corrupted by encryption/decryption", startedWith, endedWith);
+
+            mEncryptor.saveKey(aesKeyEncryptedFile, publicKeyFile);
+            Encryptor newEncryptor = new Encryptor();
+            newEncryptor.loadKey(aesKeyEncryptedFile, privateKeyFile);
+
+            mEncryptor.decrypt(toDecryptFile, secondResultFile);
+            String firstResult = new Scanner(resultFile).toString();
+            String secondResult = new Scanner(secondResultFile).toString();
+        Assert.assertEquals("Re-loaded aes key yields different decryption result", firstResult, secondResult);
+        } catch (IOException | GeneralSecurityException e) {
             Assert.fail(e.getMessage());
             e.printStackTrace();
         }
 
-        byte[] startedWith = readFile(toEncryptFile);
-        byte[] endedWith = readFile(resultFile);
-        Assert.assertArrayEquals("String corrupted by encryption/decryption", startedWith, endedWith);
-    }
-
-    @Test
-    public void reloadKeyCheck() throws Exception
-    {
-        //TODO: Fix encryption test of aes key - Consider generating keypair using different method??
-        mEncryptor.saveKey(aesKeyEncryptedFile, publicKeyFile);
-        Encryptor newEncryptor = new Encryptor();
-        newEncryptor.loadKey(aesKeyEncryptedFile, privateKeyFile);
-
-        //int origKeySig = mEncryptor.aesKeyHash();
-        //int newKeySig = newEncryptor.aesKeyHash();
-        //Assert.assertEquals("Reloaded key's hash differs from original's", origKeySig, newKeySig);
-
-        byte[] origKey = mEncryptor.aesKey;
-        byte[] newKey = newEncryptor.aesKey;
-        Assert.assertArrayEquals("Reloaded key's hash differs from original's", origKey, newKey);
-
-        mEncryptor.decrypt(toDecryptFile, secondResultFile);
-        byte[] firstResult = readFile(resultFile);
-        byte[] secondResult = readFile(secondResultFile);
-        Assert.assertArrayEquals("Re-loaded aes key yields different decryption result", firstResult, secondResult);
-    }
-
-    private byte[] readFile(File file) {
-        byte[] fileContents = new byte[(int) file.length()];
-        FileInputStream fis;
-        try {
-            fis = new FileInputStream(file);
-            fis.read(fileContents);
-            fis.close();
-        }catch (IOException e) {
-            Assert.fail(e.getMessage());
-            e.printStackTrace();
-        }
-        return fileContents;
     }
 }
